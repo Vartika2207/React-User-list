cbc93db0c9929f041cda678dd564b81cd3041930
Vartika Shakya <65726781+Vartika2207@users.noreply.github.com>
2023-01-05 02:44:30 +0530

    Delete src directory

--- src/components/Helpers/Wrapper.js
+++
@@ -1,6 +0,0 @@
-const Wrapper = (props) => {
-    return (props.children);/*children prop holds all the content passed between the opening and closing tag
-                              off your custom component.*/
-};
-
-export default Wrapper;
